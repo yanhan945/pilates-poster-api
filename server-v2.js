5e177f1eac0caf0303fd4f36e23889d0524a76d9
yanhan945 <1464155122@qq.com>
2026-05-27 23:48:43 +0800

    Update server-v2.js
    logo上传

--- server-v2.js
+++ server-v2.js
@@ -13,7 +13,7 @@ const puppeteer = require("puppeteer");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 app.use("/posters", express.static(path.join(__dirname, "posters")));
 app.use("/fonts", express.static(path.join(__dirname, "node_modules", "@fontsource", "noto-sans-sc", "files")));
